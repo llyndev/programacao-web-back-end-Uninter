4cf5282dbac5103911c2b7652d39dbbd00c85559
llyndev <weslley.alves3310@gmail.com>
2026-08-02 17:04:46 -0300

    feat: criado endpoints para adicionar e consultar produtos

---
+++ src/main/java/baozi/store/baozi/controller/ProdutoController.java
@@ -0,0 +1,36 @@
+package baozi.store.baozi.controller;
+
+import baozi.store.baozi.model.Produto;
+import baozi.store.baozi.service.ProdutoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/produto")
+public class ProdutoController {
+
+    private final ProdutoService produtoService;
+
+    public ProdutoController(ProdutoService produtoService) { this.produtoService = produtoService; }
+
+    @GetMapping
+    public ResponseEntity<List<Produto>> getAll() {
+        return ResponseEntity.ok(produtoService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getById(@PathVariable Long id) {
+        Produto produto = produtoService.getById(id);
+        return ResponseEntity.ok(produto);
+    }
+
+    @PostMapping
+    public ResponseEntity<Produto> addProduto(@RequestBody Produto produto) {
+        return ResponseEntity.ok(produtoService.addProduto(produto));
+    }
+}
