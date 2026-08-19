7af60b3e1e4bdfbac3853d2b3cef98a5807ec80f
llyndev <weslley.alves3310@gmail.com>
2026-08-18 23:49:39 -0300

    refactor: alterado o return para ClienteResponse e retirado parte logica do controller

--- src/main/java/baozi/store/baozi/controller/ClienteController.java
+++ src/main/java/baozi/store/baozi/controller/ClienteController.java
@@ -1,5 +1,6 @@
 package baozi.store.baozi.controller;
 
+import baozi.store.baozi.dto.ClienteResponse;
 import baozi.store.baozi.model.Cliente;
 import baozi.store.baozi.service.ClienteService;
 import org.springframework.http.HttpStatus;
@@ -21,46 +22,23 @@ public class ClienteController {
     }
 
     @PostMapping
-    public ResponseEntity<Cliente> createCliente(@RequestBody Cliente cliente) {
+    public ResponseEntity<ClienteResponse> createCliente(@RequestBody Cliente cliente) {
         return ResponseEntity.ok(clienteService.createCliente(cliente));
     }
 
     @GetMapping
-    public ResponseEntity<List<Cliente>> getAllCliente() {
+    public ResponseEntity<List<ClienteResponse>> getAllCliente() {
         return ResponseEntity.ok(clienteService.getAll());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getById(@PathVariable Long id) {
-        Cliente cliente = clienteService.getById(id);
-
-        if (cliente == null) {
-            Map<String, String> error = new HashMap<>();
-            error.put("message", "Cliente não encontrado.");
-
-            return ResponseEntity
-                    .status(HttpStatus.NOT_FOUND)
-                    .body(error);
-        }
-
-        return ResponseEntity.ok(cliente);
+        return ResponseEntity.ok(clienteService.getById(id));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteById(@PathVariable Long id) {
-        Cliente cliente = clienteService.getById(id);
-
-        if (cliente == null) {
-            Map<String, String> error = new HashMap<>();
-            error.put("message", "Cliente não encontrado.");
-
-            return ResponseEntity
-                    .status(HttpStatus.NOT_FOUND)
-                    .body(error);
-        }
-
         clienteService.deleteCliente(id);
-
         return ResponseEntity.ok().build();
     }
 
