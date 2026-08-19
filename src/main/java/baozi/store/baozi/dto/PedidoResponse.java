79ff85cd24714b83934251736d2849a33e85d4a4
llyndev <weslley.alves3310@gmail.com>
2026-08-18 23:52:04 -0300

    feat: adicionado construtor no pedidoresponse

--- src/main/java/baozi/store/baozi/dto/PedidoResponse.java
+++ src/main/java/baozi/store/baozi/dto/PedidoResponse.java
@@ -1,11 +1,20 @@
 package baozi.store.baozi.dto;
 
+import baozi.store.baozi.model.Pedido;
+
 import java.math.BigDecimal;
 
 public record PedidoResponse(
         String nomeCliente,
         String nomeProduto,
         BigDecimal precoProduto,
-        Integer quantidade
+        Integer quantidade,
+
+        boolean status
 ) {
+
+    public PedidoResponse(Pedido pedido){
+        this(pedido.getCliente().getNome(), pedido.getProduto().getNome(), pedido.getProduto().getPreco(), pedido.getQuantidade(), pedido.getStatus());
+    }
+
 }
